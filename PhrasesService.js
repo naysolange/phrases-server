858bfb12f9dbb6641ae817a8b95d0951765ecd8f
naysolange <naylaportas@gmail.com>
2024-02-26 17:33:19 -0300

    Mejora textos por mostrar

--- PhrasesService.js
+++ PhrasesService.js
@@ -52,20 +52,23 @@ class PhrasesService {
             reverse: true
         });
 
-        const UNKNOWN_LOCATION_TEXT = 'algún lugar del mundo';
+        const UNKNOWN_LOCATION_TEXT = 'algún lugar';
         const UNKNOWN_DEVICE_TEXT = 'algún dispositivo';
-        const UNKNOWN_OS_TEXT = 'algún navegador';
+        const UNKNOWN_OS_TEXT = 'algún sistema operativo';
+        const UNKNOWN_BROWSER_TEXT = 'algún navegador';
 
         const mapDataToJSON = (row) => {
-            const unknownLocationText = UNKNOWN_LOCATION_TEXT;
-            const unknownDeviceText = UNKNOWN_DEVICE_TEXT;
-            const unknownOSText = UNKNOWN_OS_TEXT;
+            const browser = row.get('browser');
+            const os = row.get('os');
+    
+            const browserPart = browser ? `${browser}` : UNKNOWN_BROWSER_TEXT;
+            const osPart = os ? `${os}` : UNKNOWN_OS_TEXT;
 
             return {
                 phrase: row.get('phrase'),
-                location: row.get('city') || row.get('region') || row.get('country') ? `${row.get('city') || unknownLocationText}, ${row.get('region') || unknownLocationText}, ${row.get('country') || unknownLocationText}` : unknownLocationText,
-                device: row.get('device_model') || row.get('device_type') || row.get('device_vendor') ? `${row.get('device_model') || unknownDeviceText}, ${row.get('device_type') || unknownDeviceText}, ${row.get('device_vendor') || unknownDeviceText}` : unknownDeviceText,
-                os: `${row.get('browser') || unknownOSText} ${row.get('browser_version') || unknownOSText}, ${row.get('os') || unknownOSText} ${row.get('os_version') || unknownOSText}`
+                location: row.get('city') || row.get('region') || row.get('country') ? `${row.get('city') || UNKNOWN_LOCATION_TEXT}, ${row.get('region') || UNKNOWN_LOCATION_TEXT}, ${row.get('country') || UNKNOWN_LOCATION_TEXT}` : UNKNOWN_LOCATION_TEXT,
+                device: row.get('device_model') || row.get('device_type') || row.get('device_vendor') ? `${row.get('device_model') || UNKNOWN_DEVICE_TEXT}, ${row.get('device_type') || UNKNOWN_DEVICE_TEXT}, ${row.get('device_vendor') || UNKNOWN_DEVICE_TEXT}` : UNKNOWN_DEVICE_TEXT,
+                os: `${osPart}, ${browserPart}`
             };
         };
 
