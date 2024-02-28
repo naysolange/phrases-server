eafab6d774de9fe2bb5746fd59deeda349f79e2a
naysolange <naylaportas@gmail.com>
2024-02-28 16:06:11 -0300

    Modifica textos a mostrar cuando vienen vacíos

--- PhrasesService.js
+++ PhrasesService.js
@@ -52,22 +52,28 @@ class PhrasesService {
             reverse: true
         });
 
-        const UNKNOWN_LOCATION_TEXT = 'algún lugar';
+        const UNKNOWN_CITY_TEXT = 'de algún lugar';
+        const UNKNOWN_COUNTRY = 'algún país';
         const UNKNOWN_DEVICE_TEXT = 'algún dispositivo';
-        const UNKNOWN_OS_TEXT = 'algún sistema operativo';
-        const UNKNOWN_BROWSER_TEXT = 'algún navegador';
+        const UNKNOWN_OS_TEXT = '¿sistema operativo?';
+        const UNKNOWN_BROWSER_TEXT = '¿qué navegador?';
+        const EMPTY_PHRASE = 'no lo sé';
 
         const mapDataToJSON = (row) => {
             const browser = row.get('browser');
             const os = row.get('os');
-    
+            const deviceModel = row.get('device_model') || UNKNOWN_DEVICE_TEXT;
+            const deviceType = row.get('device_type') || UNKNOWN_DEVICE_TEXT;
+            const deviceVendor = row.get('device_vendor') || UNKNOWN_DEVICE_TEXT;
+   
             const browserPart = browser ? `${browser}` : UNKNOWN_BROWSER_TEXT;
             const osPart = os ? `${os}` : UNKNOWN_OS_TEXT;
+            const device = (deviceModel === deviceType && deviceType === deviceVendor) ? deviceModel : `${deviceModel}, ${deviceType}, ${deviceVendor}`
 
             return {
-                phrase: row.get('phrase'),
-                location: row.get('city') || row.get('region') || row.get('country') ? `${row.get('city') || UNKNOWN_LOCATION_TEXT}, ${row.get('region') || UNKNOWN_LOCATION_TEXT}, ${row.get('country') || UNKNOWN_LOCATION_TEXT}` : UNKNOWN_LOCATION_TEXT,
-                device: row.get('device_model') || row.get('device_type') || row.get('device_vendor') ? `${row.get('device_model') || UNKNOWN_DEVICE_TEXT}, ${row.get('device_type') || UNKNOWN_DEVICE_TEXT}, ${row.get('device_vendor') || UNKNOWN_DEVICE_TEXT}` : UNKNOWN_DEVICE_TEXT,
+                phrase: `${row.get('phrase') || EMPTY_PHRASE}`,
+                location: `${row.get('city') || UNKNOWN_CITY_TEXT}, ${row.get('country') || UNKNOWN_COUNTRY}`,
+                device: `${device}`,
                 os: `${osPart}, ${browserPart}`
             };
         };
