7573fbb6fb6f6cf2df006e9b6909d2165c4ae08b
naysolange <naylaportas@gmail.com>
2024-07-01 12:49:36 -0300

    Cambia textos default

--- PhrasesService.js
+++ PhrasesService.js
@@ -55,8 +55,8 @@ class PhrasesService {
 
         const UNKNOWN_CITY_TEXT = 'de algún lugar';
         const UNKNOWN_COUNTRY = 'algún país';
-        const UNKNOWN_DEVICE_TEXT = 'algún dispositivo';
-        const UNKNOWN_OS_TEXT = 'un sistema operativo';
+        const UNKNOWN_DEVICE_TEXT = 'un dispositivo';
+        const UNKNOWN_OS_TEXT = 'un sistema';
         const UNKNOWN_BROWSER_TEXT = 'algún navegador';
         const EMPTY_PHRASE = 'no lo sé';
 
